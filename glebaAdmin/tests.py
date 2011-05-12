c53bb90d40f7ff287f6dc182485ed7201eaf0e5f
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-05-12 16:10:18 +0800

    tests.py: Added a simple unit test.
    this serves as a model for creating other tests.

--- glebaAdmin/tests.py
+++ glebaAdmin/tests.py
@@ -6,18 +6,47 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
+from django.test import Client
+from django.contrib.auth.models import User
+from glebaAdmin.models import *
+from glebaAdmin.views import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+################################################################################
+# View tests
+################################################################################
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TestReportViews(TestCase):
+    def setUp(self):
+        joe = User.objects.create_user('joe', 'joe@doe.com', 'doe')
+        joe.is_staff = True
+        joe.save()
+        self.c = Client()
+        login = self.c.login(username = 'joe', password = 'doe')
+        self.assertTrue(login)
+        self.picker = get_object_or_404(Picker, pk = 1)
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_views_daily_totals(self):
+        variety = get_object_or_404(Mushroom, pk = 1)
+        batch = get_object_or_404(Batch, pk = 1)
+        # Create some boxes
+        for i in range(20):
+            timestamp = datetime.datetime(2011, 05, i+10)
+            for j in range(1000):
+                box = Box(initialWeight = 4.050 + j,
+                          finalWeight = 4.025,
+                          timestamp = timestamp,
+                          contentVariety = variety,
+                          picker = self.picker,
+                          batch = batch)
+                box.save()
+        start_date = datetime.datetime(2011, 05, 01)
+        end_date = datetime.datetime(2011, 05, 11)
+        daily_totals = self.picker.daily_totals(start_date, end_date)
+        self.assertEquals(len(daily_totals), 1)
+        # test the report view
+        for i in range(20000):
+            self.c.post('/report/picker/1', {
+                'startDate': '01-05-2011',
+                'endDate':   '11-05-2011',
+            })
