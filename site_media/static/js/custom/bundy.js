6ad1cc714f6beb7c49dc076cb7dd185261126ff8
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-07-10 08:45:33 +0800

    Added 'clear' button to bundy.
    This new button clears out the entire picker field so that the
    user is not forced to click 'backspace' multiple times.
    I've also added sound feedback to each button press for increased
    usability.

--- site_media/static/js/custom/bundy.js
+++ site_media/static/js/custom/bundy.js
@@ -7,12 +7,20 @@ Bundy.submitForm = function(view) {
 }
 
 Bundy.appendNumber = function(x) {
+    document.getElementById('button_audio').play();
     var pickerIDInput = document.getElementById('picker_id');
     pickerIDInput.value += x;
 }
 
 Bundy.backspace = function() {
+    document.getElementById('button_audio').play();
     var pickerIDInput = document.getElementById('picker_id');
     pickerIDInput.value =
         pickerIDInput.value.substring(0, pickerIDInput.value.length - 1);
+}
+
+Bundy.clear = function() {
+    document.getElementById('button_audio').play();
+    var pickerIDInput = document.getElementById('picker_id');
+    pickerIDInput.value = '';
 }
