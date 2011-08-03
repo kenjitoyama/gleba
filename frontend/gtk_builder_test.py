0312ba380f823dbb32c653fe56ea4b14e00e6a7a
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-08-03 15:22:40 +0800

    GUI GTK3: Builder interface now has some data.
    The skeleton is beginning to take some shape. It now contains the
    initial data necessary for proper operation.

--- frontend/gtk_builder_test.py
+++ frontend/gtk_builder_test.py
@@ -1,4 +1,8 @@
 from gi.repository import Gtk
+import utils
+import config
+
+DB = utils.DBAPI()
 
 class MainWindow:
     def __init__(self):
@@ -6,6 +10,51 @@ class MainWindow:
         self.builder.add_from_file('gui.ui')
         self.window = self.builder.get_object('window')
         self.builder.connect_signals(self)
+        self.batches   = DB.get_active_batches_xml()
+        self.pickers   = DB.get_active_pickers_xml()
+        self.varieties = DB.get_active_varieties_xml()
+        # add batches
+        batch_combo_box = self.builder.get_object('batch_combo_box')
+        batch_text_format = 'Batch No. {} ({}) Room {}'
+        for batch in self.batches:
+            batch_combo_box.append_text(batch_text_format.format(
+                batch[0], batch[1], batch[2]
+            ))
+        # add pickers
+        picker_total = len(self.pickers)
+        cols = config.PICKER_COLS
+        rows = picker_total/cols + int(picker_total%cols != 0)
+        picker_vbox = self.builder.get_object('picker_vbox')
+        for row in range(0, rows):
+            hbox = Gtk.HBox()
+            for col in range(0, cols):
+                idx = cols*row + col
+                if idx >= picker_total:
+                    break
+                text = '{0}. {1}'.format(self.pickers[idx][0],
+                                         self.pickers[idx][1])
+                button = Gtk.Button(label = text)
+                button.set_size_request(14, 10)
+                button.connect('clicked', self.select_picker_callback, idx)
+                hbox.add(button)
+            picker_vbox.add(hbox)
+        # add varieties
+        variety_total = len(self.varieties)
+        cols = config.VARIETY_COLS
+        rows = variety_total/cols + int(variety_total%cols != 0)
+        variety_vbox = self.builder.get_object('variety_vbox')
+        for row in range(0, rows):
+            hbox = Gtk.HBox()
+            for col in range(0, cols):
+                idx = cols*row + col
+                if idx >= variety_total:
+                    break
+                text = '{0}'.format(self.varieties[idx][1])
+                button = Gtk.Button(label = text)
+                button.set_size_request(14, 15)
+                button.connect('clicked', self.select_variety_callback, idx)
+                hbox.add(button)
+            variety_vbox.add(hbox)
 
     def exit_callback(self, widget, data = None):
         Gtk.main_quit()
@@ -18,6 +67,12 @@ class MainWindow:
         pass
 
     def modify_history_callback(self, widget, data):
+        pass
+
+    def select_picker_callback(self, widget, data):
+        pass
+
+    def select_variety_callback(self, widget, data):
         pass
 
 if __name__ == '__main__':
