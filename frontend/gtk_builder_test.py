101dbbcc0f13e73aa5e1ba919f8bd5597b558b52
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-08-03 15:04:36 +0800

    GUI GTK3: Added Edit Window to XML skeleton.
    The skeleton now properly launches the Edit Window. We have just
    method stubs at the moment, but they will eventually do something
    useful.

--- frontend/gtk_builder_test.py
+++ frontend/gtk_builder_test.py
@@ -2,18 +2,22 @@ from gi.repository import Gtk
 
 class MainWindow:
     def __init__(self):
-        builder = Gtk.Builder()
-        builder.add_from_file('gui.ui')
-        self.window = builder.get_object('window')
-        builder.connect_signals(self)
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('gui.ui')
+        self.window = self.builder.get_object('window')
+        self.builder.connect_signals(self)
 
     def exit_callback(self, widget, data = None):
         Gtk.main_quit()
 
     def open_edit_window(self, widget, data = None):
-        pass
+        edit_window = self.builder.get_object('edit_window')
+        edit_window.show_all()
 
     def commit_callback(self, widget, data = None):
+        pass
+
+    def modify_history_callback(self, widget, data):
         pass
 
 if __name__ == '__main__':
