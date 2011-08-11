1c3850a2f74cebc0f4ef61c376bcbdc67532f859
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-08-11 22:39:15 +0800

    Removed junk in static and media folders.
    There was some leftovers in these two folders. Things like gnuplot
    files (.png) and CSV files.
    I've also "fixed" the path for the admin media, which now should
    not be found and by default should load the default media in the
    django distribution.

--- settings.py
+++ settings.py
@@ -65,7 +65,7 @@ MEDIA_URL = '{0}{1}media/'.format(BASE_URL, STATIC_URL)
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '{0}admin/'.format(MEDIA_URL)
+ADMIN_MEDIA_PREFIX = '{0}admin/'.format(STATIC_URL)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '3g29@+eq399*b!cvpy6!nez$+k*+)ijh)oht2z2m0vqvpw(50x'
