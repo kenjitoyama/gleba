c0a5f29d26d4acafa070e06ec6617a7df28d44e6
Daniel Kenji Toyama <kenji.toyama@gmail.com>
2011-08-15 10:50:31 +0800

    apps/utilities: Removed unused function.
    We are not using last_month() anymore.

--- apps/utilities.py
+++ apps/utilities.py
@@ -28,13 +28,6 @@ Purpose:
 """
 import datetime
 
-def last_month():
-    """
-    Returns a list of the last 31 days including today
-    """
-    return [datetime.date.today() - datetime.timedelta(days = i)
-            for i in range(31)]
-
 def date_range(start_date, end_date):
     """
     Returns a list of the days between startDate and endDate inclusive.
